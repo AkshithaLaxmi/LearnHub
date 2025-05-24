388d904c98c1cab042be76994256e6b1ed8fa639
rxhul18 <rahulshah0728@gmail.com>
2025-05-24 20:31:21 +0530

    Final chances testing, change verifying api url and few fix in backend

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -4,7 +4,7 @@ axios.defaults.withCredentials = true;
 
 export async function getCurrentUser() {
   try {
-    const res = await axios.get("http://localhost:3050/api/v1/me",{withCredentials:true});
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/me`,{withCredentials:true});
     return res.data; // contains user info
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (err) {
